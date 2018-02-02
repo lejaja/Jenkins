c3ced6e267401cb57502da08d969e5a0a24bc18d
lejaja <ngoukamcharly@yahoo.fr>
2018-02-02 13:45:12 +0100

    My First Commit

---
+++ src/main/java/com/kdp/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.kdp.java;
+
+public class HelloWorld {
+
+	/**
+	 * @author Kshitij Pawar	
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// mon deuxieme commit
+		Greeting obj = new Greeting();
+		System.out.println(obj.greet("Charly"));
+	}	
+}
